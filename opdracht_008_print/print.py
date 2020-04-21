765fea45614f3dffb40ca68ea273454dc98eae39
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-04-21 13:26:09 +0200

    did excercise 8 & 9

---
+++ opdracht_008_print/print.py
@@ -0,0 +1,11 @@
+""" 1. gebruik het `print` statement om allerlei dingen te printen / OK
+2. print getallen / OK
+3. print strings (`"Hallo"`) / OK
+4. print een combinatie van die dingen / OK
+5. Probeer dit met Python files en in een REPL / OK
+6. Is het enige handiger dan het andere? /REPL is sneller?? """
+
+
+print('hello world!!', 6+6)
+print(1+9)
+print('test')
---
+++ opdracht_009_variabele/variabele.py
@@ -0,0 +1,15 @@
+""" 1. maak een variabele aan en stop daar de waarde `3` in /OK
+2. maak een variabele aan en stop daar de waarde `"Hallo"` in /OK
+3. maak een variabele aan en stop daar de waarde `True` in /OK
+4. print losse variabelen uit met `print` /OK
+5. print meerdere variabelen tegelijk uit met `print` /OK
+6. print meerdere dingen tegelijk uit: variabelen én expliciete waardes, geeft dat een ander resultaat? /Nope! """
+
+number = 3
+welcome = 'Hallo'
+boolean = True
+print(number)
+print(welcome)
+print(boolean)
+print(number, welcome, boolean)
+print(number, welcome, boolean, 'test', 1, 1+1, )
