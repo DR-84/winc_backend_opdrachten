455fb6e55b27483de76d1620eff38c92c8b0bb6a
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-04-23 12:47:55 +0200

    finished cow excercise

--- opdracht_015_boolean_logical_operators_if_statement/boolean_logical_operators_if_statement.py
+++ opdracht_015_boolean_logical_operators_if_statement/boolean_logical_operators_if_statement.py
@@ -18,39 +18,59 @@
 
 rain = False
 sun = True
-daytime = True
-cows_are_out = True
-too_windy = True
-poop_overload = True
-got_milk = True
+daytime = False
+cows_are_out = False
+too_windy = False
+poop_overload = False
+got_milk = False
 got_pumpkin_spice = True
 
 get_cows_inside = 'haal de koeien naar binnen'
 milk_the_cows = 'melk de koeien'
-cows_can_stay_out = 'koeien kunnen buiten blijven'
-
+cows_can_stay_out = 'koeien kunnen naar buiten'
+fertilize_the_land = 'bemest het land'
+mow_the_lawn = 'maai het gras voor hooi'
 
 # Als de koeien gemolken moeten worden dan moeten ze naar binnen gehaald worden en vervolgens gemolken.
-if cows_are_out and got_milk == True:
+if cows_are_out and got_milk:
     print(get_cows_inside + ' & ' + milk_the_cows+'.')
-elif cows_are_out == False and got_milk == True:
+elif not cows_are_out and got_milk:
     print(milk_the_cows)
 else:
     print(cows_can_stay_out)
 
 # Als de koeien 's nachts op het weiland staan en het regent dan moeten ze naar binnen gehaald worden.
-if cows_are_out and rain == True and daytime == False:
+if cows_are_out and rain and not daytime:
     print(get_cows_inside)
 else:
     print(cows_can_stay_out)
 
 # Als de koeien op het weiland staan en er staat veel wind dan moeten ze binnen gehaald worden, als ze ook nog gemolken moeten worden, doe dat dan vervolgens.
-if cows_are_out and too_windy == True and got_milk == False:
+if cows_are_out and too_windy and not got_milk:
     print(get_cows_inside)
-elif cows_are_out and too_windy and got_milk == True:
+elif cows_are_out and too_windy and got_milk:
     print(get_cows_inside + ' & ' + milk_the_cows+'.')
 else:
     print(cows_can_stay_out)
 
 # Als de gierput vol en het regent moet het land bemest worden. Denk aan de koeien.
-if
+if poop_overload and rain and cows_are_out:
+    print(get_cows_inside + ' & ' + fertilize_the_land)
+elif poop_overload and rain and not cows_are_out:
+    print(fertilize_the_land)
+else:
+    print(cows_can_stay_out)
+
+# Als het herfst is en de zon schijnt moet het gras gemaaid worden. Denk aan de koeien.
+if sun and got_pumpkin_spice and not cows_are_out:
+    print(mow_the_lawn)
+elif sun and got_pumpkin_spice and cows_are_out:
+    print(get_cows_inside + ' & ' + mow_the_lawn)
+else:
+    print(cows_can_stay_out)
+
+# Als het herfst is en de zon schijnt moeten de koeien naar buiten, tenzij ze al buiten zijn.
+if sun and got_pumpkin_spice and not cows_are_out:
+    print(cows_can_stay_out)
+else:
+    print(get_cows_inside)
