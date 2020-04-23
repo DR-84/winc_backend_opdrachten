56162a8b624515a8692c49327340c9d1882d5707
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-04-23 10:11:56 +0200

    changed excercise numbers, added excercise 014

---
+++ opdracht_014_expression_statements/expressions_statements.py
@@ -0,0 +1,12 @@
+3  # expression
+3 + 3  # expression
+prijs = 5  # assingnment statement
+aantal = 3  # assingnment statement
+prijs * aantal  # expression
+totaalPrijs = prijs * aantal  # assingnment statement met een expression
+totaalPrijs  # ??
+print(totaalPrijs)  # statement
+frank = "Frank Rijkaard"  # assingnment statement
+print(f'Hallo {frank}')  # statement
+f'Hoi {frank}'  # expression
+f'Zeg {frank}, jouw voornaam heeft {len(frank[:5])} karakters.'  # expression
