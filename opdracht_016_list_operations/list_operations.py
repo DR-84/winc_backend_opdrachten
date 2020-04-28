3e0025cb6caed5625e32e02cf7657fa2cc707335
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-04-28 09:48:59 +0200

    added all members as lists

---
+++ opdracht_016_list_operations/list_operations.py
@@ -0,0 +1,34 @@
+
+shane_embury = ["Shane Embury", ["bass", "backing vocals"], [1987, 2021]]
+mark_greenway = ["Mark \"Barney\"Greenway",
+                 "lead vocals",  [[1989, 1996], [1997, 2021]]]
+danny_herrera = ["Danny Herrera", "drums", [1991, 2021]]
+mitch_harris = ["Mitch Harris", ["guitars, backing vocals"]]
+nicholas_bullen = ["Nicholas \"Nik Napalm\" Bullen",
+                   ["lead vocals", "bass"], [1981, 1986]]
+miles_ratledge = ["Miles \"Rat\" Ratledge", "drums", [1981, 1985]]
+simon_oppenheimer = ["Simon \"Si O\" Oppenheimer", "guitars", [1981, 1982]]
+graham_robertson = ["Graham \"Grayhard\" Robertson",
+                    ["guitars", "bass"], [1982, 1985]]
+daryl_fedeski = ["Daryl \"Daz F\" Fedeski", "guitars", 1982]
+finbar_quinn = ["Finbar \"Fin\" Quinn", "bass", [1983, 1984]]
+marian_williams = ["Marian Williams", "lead vocals", 1984]
+damien_errington = ["Damien Errington", "guitars", 1985]
+justin_broadrick = ["Justin Broadrick", [
+    "guitars", "backing and lead vocals"], [1985, 1986]]
+peter_shaw = ["Peter \"P-Nut\" Shaw", "bass", 1985]
+mick_harris = ["Mick Harris", ["drums", "backing vocals"], [1985, 1991]]
+jim_whitely = ["Jim Whitely", "bass", [1986, 1987]]
+frank_healy = ["Frank Healy", "guitars", 1986]
+bill_steer = ["Bill Steer", "guitars", [1987, 1989]]
+lee_dorian = ["Lee Dorrian", "lead vocals", [1987, 1989]]
+jesse_pintado = ["Jesse Pintado", "guitars",  [1989, 2004]]
+phil_vane = ["Phil Vane", "lead vocals", [1996, 1997]]
+erik_burke = ["Erik Burke", "guitars", 2015]
+jesper_liverod = ["Jesper Liveröd", "bass", 2017]
+
+# Het jaar is 2003. Mark Greenway doet lead vocals, Jesse Pintado speelt gitaar,
+# Mitch Harris speelt gitaar en doet backing vocals, Shane Emury speelt basgitaar
+# en Danny Herera speelt de drums.
+print("het jaar is 1981 " + nicholas_bullen[0] + " doet "+nicholas_bullen[1][0] + "," + nicholas_bullen[1][1]+". "+simon_oppenheimer[0]+" speelt "
+      + simon_oppenheimer[1])
