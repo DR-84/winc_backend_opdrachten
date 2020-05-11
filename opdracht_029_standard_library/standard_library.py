11a02cfb6d5eba9734d9cb41516850082c08124a
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-05-11 16:14:10 +0200

    added lots of stuff

--- opdracht_029_standard_library/standard_library.py
+++ opdracht_029_standard_library/standard_library.py
@@ -1,37 +1,30 @@
-lst1 = [1, 2, 3]
-lst2 = lst1
-lst1[1] = 6
-result = lst1[1] + lst2[1]
-print(result)
-# Wat is de waarde van result?
-# Antwoord: result = 12
-# En waarom is dat de waarde van result?
-# Antwoord: list2 is list1, dan list1 tweede value aangepast naar 6,
-# tel lijst[1] bij elkaar op is 12.
+import secret
+import os
+import shutil
+
+# https://docs.python.org/2/library/argparse.html (maybe check this later)
+# opdracht 1:
+
+folder = r"C:\Users\ddder\OneDrive\Bureaublad\Picapica"
 
 
-dict1 = {"a": 1, "b": 2}
-dict2 = dict1
-dict1["a"] = 8
-result = dict1["a"] + dict2["a"]
-print(result)
-# Wat is de waarde van result?
-# Antwoord: result = 16
-# En waarom is dat de waarde van result?
-# Antwoord: 8 + 8
+def files_in_dir(path):
+    for i in os.listdir(path):
+        print(i)
 
-lst3 = [{"c": 3, "d": 4}, {"c": 5, "d": 6}]
-lst4 = lst3
-lst3[0]["c"] = 7
-result = 0
-for lst in [lst3, lst4]:
-    for dct in lst:
-        for val in dct.values():
-            result += val
-            print(result)
-# Wat is de waarde van result?
-# Antwoord: result = 44 of 11,22,33,44 of 7,11,16,22,29,33,38,44 (ligt aan de plek van je print() )
-# En waarom is dat de waarde van result?
-# Antwoord: Je past lst3 eerste item in eerste dict aan naar 7.
-# de rest blijft het zelfde en dan voor elke value in de dicts in de lists
-# tel je bij result op
+
+# files_in_dir(folder)
+
+# opdracht 2:
+
+
+def file_is_juf(files):
+    for i in os.listdir(files):
+        just_number = os.path.splitext(i)[0]
+        if int(just_number) % 7 == 0 or "7" in just_number:
+            shutil.move(files + r"\\" + i, secret.juf)
+        else:
+            shutil.move(files + r"\\" + i, secret.rest)
+
+
+file_is_juf(secret.files)
