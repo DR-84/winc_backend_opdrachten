e6871ff94b94c8d651314a4529bb081f5a40ba20
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-05-11 16:18:09 +0200

    deleted mistake

--- opdracht_029_standard_library/secret.py
+++
@@ -1,3 +0,0 @@
-juf = r"C:\Users\ddder\OneDrive\Bureaublad\Winc\winc_backend_code\winc_backend_opdrachten\opdracht_029_standard_library\new_directory\juf"
-rest = r"C:\Users\ddder\OneDrive\Bureaublad\Winc\winc_backend_code\winc_backend_opdrachten\opdracht_029_standard_library\new_directory\rest"
-files = r"C:\Users\ddder\OneDrive\Bureaublad\Winc\winc_backend_code\winc_backend_opdrachten\opdracht_029_standard_library\new_directory\files"
